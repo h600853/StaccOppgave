799b4cc8fee2b78a50da2bd5735aa19696b4d4ef
h600853 <h600853@stud.hvl.no>
2023-09-10 19:12:48 +0200

    feat: SavingGoalsController

---
+++ WebApplication1/Controllers/SavingGoalsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SavingGoalsController : ControllerBase
+    {
+        private readonly ILogger<SavingGoalsController> _logger;
+        private readonly MydatabaseContext dx = new MydatabaseContext();
+        private LocalView<SavingGoal> savingGoals;
+        public SavingGoalsController(ILogger<SavingGoalsController> logger)
+        {
+            this.savingGoals = dx.SavingGoals.Local;
+            dx.SavingGoals.Load();
+
+            _logger = logger;
+        }
+        [HttpGet("GetSavingGoalsForUser")]
+        public IActionResult GetSavingGoalsForUser(int userId)
+        {
+            List<SavingGoal> savingGoals = dx.SavingGoals.Where(x => x.UserId == userId).ToList();
+
+            if (savingGoals.Count == 0)
+            {
+                return NotFound(new { message = "No saving goals were found for the user" });
+            }
+
+            return Ok(new { message = "Saving goals were found", savingGoalsData = savingGoals });
+        }
+        [HttpPost("CreateSavingGoal")]
+        public IActionResult CreateSavingGoal(int userid, string title, string description, decimal price)
+        {
+            SavingGoal newSG = new SavingGoal();    
+            newSG.UserId = userid;
+            newSG.Title = title;
+            newSG.Description = description;
+            newSG.Price = price;
+
+            savingGoals.Add(newSG);
+            dx.SaveChanges();   
+            return Ok(new {message =  "A new savingGoal has been created.", savingGoalData = newSG});
+        }
+
+        [HttpPut("UpdateSavingGoal")]
+        public IActionResult UpdateSavingGoal(int goalId, int userId, string? title, string? description, decimal? price)
+        {
+            
+            SavingGoal? savingGoal = dx.SavingGoals.FirstOrDefault(x => x.GoalId == goalId && x.UserId == userId);
+
+            if (savingGoal == null)
+            {
+                return NotFound(new { message = "SavingGoal was not found" });
+            }
+
+            if (title != null)
+            {
+                savingGoal.Title = title;
+            }
+
+            if (description != null)
+            {
+                savingGoal.Description = description;
+            }
+
+            if (price != null)
+            {
+                savingGoal.Price = price.Value;
+            }
+
+            dx.SaveChanges();
+
+            return Ok(new { message = "SavingGoal was updated", savingGoalData = savingGoal });
+        }
+        [HttpDelete("DeleteGoal")]
+        public IActionResult DeleteGoal(int goalId)
+        {
+            SavingGoal? savingGoal = dx.SavingGoals.FirstOrDefault(x => x.GoalId == goalId);
+            if (savingGoal == null)
+            {
+                return NotFound(new { message = "SavingGoal was not found" });
+            }
+            savingGoals.Remove(savingGoal);
+            dx.SaveChanges();
+            return Ok(new { message = "SavingGoal was deleted", savingGoalData = savingGoal });
+        }
+
+    }
+}
