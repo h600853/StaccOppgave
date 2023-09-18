95efc4b577ea93d7db5a7cc0226982f65d014f62
h600853 <h600853@stud.hvl.no>
2023-09-18 18:20:45 +0200

    feat: Homepage

--- StaccOppgave/UserService.cs
+++ StaccOppgave/UserService.cs
@@ -2,12 +2,17 @@
 {
     public class UserService
     {
-        public int UserId { get; private set; }
-
+        public int? UserId { get; private set; }
+        public string? UserName { get; private set; }
         public void SetUserId(int userId)
         {
             UserId = userId;
         }
+        public void SetUserName(string userName)
+        {
+            UserName = userName;
+        }
+
 
     }
 }
