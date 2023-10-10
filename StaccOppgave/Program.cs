2094ca0eb9867b2a326f2d022f11ef549da71236
h600853 <h600853@stud.hvl.no>
2023-10-10 21:42:50 +0200

    feat: Added css and components to frontend.

--- StaccOppgave/Program.cs
+++ StaccOppgave/Program.cs
@@ -10,4 +10,5 @@ builder.Services.AddScoped<UserService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazorBootstrap();
 await builder.Build().RunAsync();
